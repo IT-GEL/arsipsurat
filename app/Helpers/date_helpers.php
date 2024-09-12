989972c62521895445ebbaf531dd5d545571f45e
IT GEL <it@gel.co.id>
2024-09-12 14:46:22 +0700

    update

--- app/Helpers/date_helpers.php
+++ app/Helpers/date_helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-if (! function_exists('monthToRoman')) {
+if (!function_exists('monthToRoman')) {
     function monthToRoman($monthNumber) {
         $romanMonths = [
             1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV',
@@ -11,7 +11,7 @@ if (! function_exists('monthToRoman')) {
     }
 }
 
-if (! function_exists('formatDateIndonesian')) {
+if (!function_exists('formatDateIndonesian')) {
     function formatDateIndonesian($date) {
         $englishDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         $indonesianDayNames = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
@@ -19,8 +19,8 @@ if (! function_exists('formatDateIndonesian')) {
         $englishMonthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         $indonesianMonthNames = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
 
-        $day = \Carbon\Carbon::parse($date)->format('l');
-        $month = \Carbon\Carbon::parse($date)->format('F');
+        $day = date('l', strtotime($date));
+        $month = date('F', strtotime($date));
 
         $dayIndex = array_search($day, $englishDayNames);
         $monthIndex = array_search($month, $englishMonthNames);
@@ -28,6 +28,6 @@ if (! function_exists('formatDateIndonesian')) {
         $indonesianDay = $dayIndex !== false ? $indonesianDayNames[$dayIndex] : 'Unknown';
         $indonesianMonth = $monthIndex !== false ? $indonesianMonthNames[$monthIndex] : 'Unknown';
 
-        return \Carbon\Carbon::parse($date)->format('j') . ' ' . $indonesianMonth . ' ' . \Carbon\Carbon::parse($date)->format('Y');
+        return date('j', strtotime($date)) . ' ' . $indonesianMonth . ' ' . date('Y', strtotime($date));
     }
 }
--- resources/views/dashboard/mss/edit.blade.php
+++ resources/views/dashboard/mss/edit.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $userName = auth()->user()->name ?? '';
-    $date = null;
+    $date = $mss->tglSurat ?? null;
     $formattedDate = '';
     $total = 'N/A';
     $records = [];
@@ -39,7 +39,7 @@
                 
                         <div class="mb-3">
                             <label for="noSurat" class="form-label">Nomor Surat</label>
-                            <input type="text" value="Ref. No:MSS/GEL/BA-{{ old('noSurat', $mss->noSurat) }}/{{ $romanMonth }}/2024"  class="form-control @error('noSurat') is-invalid @enderror" id="noSurat" name="noSurat"  required value="{{ old('noSurat') }}" disabled>
+                            <input type="text" value="Ref. No:MSS/GEL/BA-{{ old('noSurat', $mss->noSurat) }}/{{ $romanMonth }}/2024"  class="form-control @error('noSurat') is-invalid @enderror" id="noSurat" name="noSurat"  required disabled>
                             @error('noSurat')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -48,8 +48,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="pttujuan" class="form-label">PT Tujuan</label>
-                            <input type="text" value="{{ old('pttuuan', $mss->pttujuan) }}" class="form-control @error('pttujuan') is-invalid @enderror" placeholder="Isi PT Tujuan..."
-                                id="pttujuan" name="pttujuan" required value="{{ old('pttujuan') }}">
+                            <input type="text" value="{{ old('pttuuan', $mss->pttujuan) }}" class="form-control @error('pttujuan') is-invalid @enderror"
+                                id="pttujuan" name="pttujuan" required>
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -59,7 +59,7 @@
                         <div class="mb-3">
                             <label for="alamat" class="form-label">Alamat PT Tujuan</label>
                             <input id="alamat" type="hidden" name="alamat" value="{{ old('alamat', $mss->alamat) }}">
-                            <trix-editor class="form-control @error('alamat') is-invalid @enderror" input="alamat" required value="{{ old('alamat') }}" placeholder="Alamat PT Tujuan"></trix-editor>
+                            <trix-editor class="form-control @error('alamat') is-invalid @enderror" input="alamat" required></trix-editor>
                             @error('keterangan')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -69,7 +69,7 @@
                         <div class="mb-3">
                             <label for="commodity" class="form-label">Commodity</label>
                             <input type="text" class="form-control @error('commodity') is-invalid @enderror" value="{{ old('commodity', $mss->commodity) }}"
-                                id="commodity" name="commodity" required value="{{ old('commodity') }}">
+                                id="commodity" name="commodity" required >
                             @error('commodity')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -79,7 +79,7 @@
                         <div class="mb-3">
                             <label for="source" class="form-label">Source</label>
                             <input type="text" class="form-control @error('source') is-invalid @enderror" value="{{ old('source', $mss->source) }}"
-                                id="source" name="source" required value="{{ old('source') }}">
+                                id="source" name="source" required >
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -89,7 +89,7 @@
                         <div class="mb-3">
                             <label for="country" class="form-label">Country of Origin</label>
                             <input type="text" class="form-control @error('country') is-invalid @enderror" value="{{ old('country', $mss->country) }}"
-                                id="country" name="country" required value="{{ old('country') }}">
+                                id="country" name="country" required>
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -98,8 +98,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="spec" class="form-label">Typical Specification</label>
-                            <input type="text" class="form-control @error('spec') is-invalid @enderror" placeholder="Specification..."
-                                id="spec" name="spec" required value="{{ old('spec') }}">
+                            <input type="text" class="form-control @error('spec') is-invalid @enderror"
+                                id="spec" name="spec" required value="{{ old('spec', $mss->spec) }}">
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -108,7 +108,7 @@
                         </div>
                         <div class="mb-3 mt-3">
                             <label for="vo" class="form-label">Validity Offer</label>
-                            <input type="date" class="form-control @error('vo') is-invalid @enderror" id="vo" name="vo" required value="{{ old('vo') }}">
+                            <input type="date" class="form-control @error('vo') is-invalid @enderror" id="vo" name="vo" value="{{ old('vo', $mss->vo) }}" required>
                             @error('vo')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -117,8 +117,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="qty" class="form-label">Quantity</label>
-                            <input type="number" class="form-control @error('qty') is-invalid @enderror" placeholder="Quantity..."
-                                id="qty" name="qty" required value="{{ old('qty') }}">
+                            <input type="number" class="form-control @error('qty') is-invalid @enderror" value="{{ old('qty', $mss->qty) }}" required
+                                id="qty" name="qty">
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -127,8 +127,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="lp" class="form-label">Loading Port</label>
-                            <input type="text" class="form-control @error('lp') is-invalid @enderror" placeholder="Loading Port..."
-                                id="lp" name="lp" required value="{{ old('lp') }}">
+                            <input type="text" class="form-control @error('lp') is-invalid @enderror" value="{{ old('lp', $mss->lp) }}"
+                                id="lp" name="lp" required>
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -137,8 +137,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="dp" class="form-label">Destination Port</label>
-                            <input type="text" class="form-control @error('dp') is-invalid @enderror" placeholder="Destination Port..."
-                                id="dp" name="dp" required value="{{ old('dp') }}">
+                            <input type="text" class="form-control @error('dp') is-invalid @enderror"
+                                id="dp" name="dp" value="{{ old('dp', $mss->dp) }}" required>
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -147,8 +147,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="cif" class="form-label">Price Scheme (CIF)</label>
-                            <input type="number" class="form-control @error('cif') is-invalid @enderror" placeholder="CIF..."
-                                id="cif" name="cif" required value="{{ old('cif') }}">
+                            <input type="number" class="form-control @error('cif') is-invalid @enderror" required value="{{ old('cif', $mss->cif) }}"
+                                id="cif" name="cif">
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -157,8 +157,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="fob" class="form-label">Price Scheme (FOB)</label>
-                            <input type="number" class="form-control @error('fob') is-invalid @enderror" placeholder="FOB..."
-                                id="fob" name="fob" required value="{{ old('fob') }}">
+                            <input type="number" class="form-control @error('fob') is-invalid @enderror" required value="{{ old('fob', $mss->fob) }}"
+                                id="fob" name="fob">
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -167,8 +167,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="freight" class="form-label">Price Scheme (FREIGHT)</label>
-                            <input type="number" class="form-control @error('freight') is-invalid @enderror" placeholder="FREIGHT..."
-                                id="freight" name="freight" required value="{{ old('freight') }}">
+                            <input type="number" class="form-control @error('freight') is-invalid @enderror" required value="{{ old('freight', $mss->freight) }}"
+                                id="freight" name="freight">
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -177,7 +177,7 @@
                         </div>
                         <div class="mb-3 mt-3">
                             <label for="shipschedule" class="form-label">Shipping Schedule</label>
-                            <input type="date" class="form-control @error('shipschedule') is-invalid @enderror" id="shipschedule" name="shipschedule" required value="{{ old('shipschedule') }}">
+                            <input type="date" class="form-control @error('shipschedule') is-invalid @enderror" required id="shipschedule" name="shipschedule" value="{{ old('shipschedule', $mss->shipschedule) }}">
                             @error('shipschedule')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -186,8 +186,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="tcd" class="form-label">Term of Coal Delivery</label>
-                            <input type="text" class="form-control @error('tcd') is-invalid @enderror" placeholder="Term of Coal Delivery..."
-                                id="tcd" name="tcd" required value="{{ old('tcd') }}">
+                            <input type="text" class="form-control @error('tcd') is-invalid @enderror" value="{{ old('tcd', $mss->tcd) }}"
+                                id="tcd" name="tcd" required>
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -196,8 +196,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="surveyor" class="form-label">Surveyor</label>
-                            <input type="text" class="form-control @error('surveyor') is-invalid @enderror" placeholder="Surveyor..."
-                                id="surveyor" name="surveyor" required value="{{ old('surveyor') }}">
+                            <input type="text" class="form-control @error('surveyor') is-invalid @enderror" value="{{ old('surveyor', $mss->surveyor) }}"
+                                id="surveyor" name="surveyor" required>
                             @error('nama')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -207,7 +207,7 @@
                        
                         <div class="mb-3 mt-3">
                             <label for="tglSurat" class="form-label">Tanggal Surat</label>
-                            <input type="date" class="form-control @error('tglSurat') is-invalid @enderror" id="tglSurat" name="tglSurat" required value="{{ old('tglSurat') }}">
+                            <input type="date" class="form-control @error('tglSurat') is-invalid @enderror" id="tglSurat" name="tglSurat" required value="{{ old('tglSurat', $mss->tglSurat) }}">
                             @error('tglSurat')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -216,7 +216,7 @@
                         </div>
                         <div class="mb-3">
                             <label for="ttd" class="form-label">Yang Menandatangai</label>
-                            <input type="text" class="form-control @error('ttd') is-invalid @enderror" id="ttd" name="ttd" placeholder="Departement Head" required value="{{ old('ttd') }}">
+                            <input type="text" class="form-control @error('ttd') is-invalid @enderror" id="ttd" name="ttd" placeholder="Departement Head" required value="{{ old('ttd'), $mss->ttd }}">
                             @error('ttd')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -225,7 +225,7 @@
                         </div>
                         <div class="mb-3">
                             <label for="namaTtd" class="form-label">Nama Yang Menandatangai</label>
-                            <input type="text" class="form-control @error('namaTtd') is-invalid @enderror" id="namaTtd" name="namaTtd" required value="{{ old('namaTtd') }}">
+                            <input type="text" class="form-control @error('namaTtd') is-invalid @enderror" id="namaTtd" name="namaTtd" required value="{{ old('namaTtd'), $mss->namaTtd }}">
                             @error('namaTtd')
                                 <div class="invalid-feedback">
                                     {{ $message }}
